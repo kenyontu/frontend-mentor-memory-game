8c3b00bdc450d4d9b960ef63307e707ca127d0ca
kenyontu <kenyontu@outlook.com>
2022-07-15 12:37:45 +0800

    add colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,26 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          400: 'hsl(37°, 98%, 54%)',
+        },
+        secondary: {
+          400: 'hsl(205°, 37%, 55%)',
+        },
+        neutral: {
+          300: 'hsl(203°, 28%, 79%)',
+          400: 'hsl(203°, 22%, 55%)',
+          700: 'hsl(205°, 30%, 27%)',
+          800: 'hsl(206°, 45%, 15%)',
+        },
+        gray: {
+          100: 'hsl(0°, 0%, 99%)',
+          300: 'hsl(0°, 0%, 95%)',
+        },
+      },
+    },
   },
   plugins: [],
 }
