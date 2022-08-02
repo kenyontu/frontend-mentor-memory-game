787149849127ea9c6c40b149c30f7430fabd3090
kenyontu <kenyontu@outlook.com>
2022-08-03 00:36:46 +0800

    add repo name to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/frontend-mentor-memory-game/',
   plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
